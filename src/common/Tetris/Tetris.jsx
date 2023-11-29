992a34c4c29acdaba93a214e1789f3a4c6695e0c
Ignacio Furió José <bichoifj@gmail.com>
2023-11-29 15:06:16 +0100

    create the table board as html tags

--- src/common/Tetris/Tetris.jsx
+++ src/common/Tetris/Tetris.jsx
@@ -3,6 +3,129 @@ import './Tetris.css'
 
 export const Tetris = () => {
   return (
-    <div className='tetrisDesign'>Tetris</div>
+    <div className='tetrisDesign'>
+      <div className='boardDesign'>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+        <div className='rowDesign'>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+          <div className='colDesign'></div>
+        </div>
+      </div>
+    </div>
   )
 }
